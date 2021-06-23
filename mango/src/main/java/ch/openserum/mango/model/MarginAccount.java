cf97a64bb771cedb7b4e6bf8aefd045173d3e691
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-06-23 11:01:15 -0700

    Add mangoGroup and owner to MarginAccount

--- mango/src/main/java/ch/openserum/mango/model/MarginAccount.java
+++ mango/src/main/java/ch/openserum/mango/model/MarginAccount.java
@@ -6,14 +6,23 @@ import lombok.Setter;
 import lombok.ToString;
 import org.p2p.solanaj.core.PublicKey;
 
+import static ch.openserum.mango.model.MangoUtils.ACCOUNT_FLAGS_SIZE_BYTES;
+import static org.p2p.solanaj.core.PublicKey.PUBLIC_KEY_LENGTH;
+
 @Builder
 @Getter
 @Setter
 @ToString
 public class MarginAccount {
 
+    // Offsets
+    private static final int MANGO_GROUP_OFFSET = ACCOUNT_FLAGS_SIZE_BYTES;
+    private static final int OWNER_OFFSET = MANGO_GROUP_OFFSET + PUBLIC_KEY_LENGTH;
+
     private PublicKey publicKey;
     private MangoAccountFlags accountFlags;
+    private PublicKey mangoGroup;
+    private PublicKey owner;
 
     public static MarginAccount readMarginAccount(final PublicKey publicKey, byte[] data) {
         if (data == null) {
@@ -34,6 +43,9 @@ public class MarginAccount {
                         .mangoSrmAccount((marginAccountFlags & 8) == 8)
                         .build()
         );
+
+        marginAccount.setMangoGroup(PublicKey.readPubkey(data, MANGO_GROUP_OFFSET));
+        marginAccount.setOwner(PublicKey.readPubkey(data, OWNER_OFFSET));
 
         return marginAccount;
     }
