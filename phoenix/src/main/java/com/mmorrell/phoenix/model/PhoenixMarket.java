a3280bbbfc100bd5163c3f44cd8e2e2f086dfded
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-11-18 18:51:28 -0800

    Phoenix: add PhoenixMarket

--- phoenix/src/main/java/com/mmorrell/phoenix/model/PhoenixMarket.java
+++ phoenix/src/main/java/com/mmorrell/phoenix/model/PhoenixMarket.java
@@ -8,14 +8,23 @@ import org.bitcoinj.core.Utils;
 @Builder
 public class PhoenixMarket {
 
+    // B trees start at offset 880
     private static final int START_OFFSET = 832;
     private long baseLotsPerBaseUnit;
     private long tickSizeInQuoteLotsPerBaseUnit;
+    private long orderSequenceNumber;
+    private long takerFeeBps;
+    private long collectedQuoteLotFees;
+    private long unclaimedQuoteLotFees;
 
     public static PhoenixMarket readPhoenixMarket(byte[] data) {
         PhoenixMarket phoenixMarket = PhoenixMarket.builder()
                 .baseLotsPerBaseUnit(Utils.readInt64(data, START_OFFSET))
                 .tickSizeInQuoteLotsPerBaseUnit(Utils.readInt64(data, START_OFFSET + 8))
+                .orderSequenceNumber(Utils.readInt64(data, START_OFFSET + 16))
+                .takerFeeBps(Utils.readInt64(data, START_OFFSET + 24))
+                .collectedQuoteLotFees(Utils.readInt64(data, START_OFFSET + 32))
+                .unclaimedQuoteLotFees(Utils.readInt64(data, START_OFFSET + 40))
                 .build();
 
         return phoenixMarket;
