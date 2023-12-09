87b74444f38b373d59fa7cf3a2553040f2410ef8
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-12-08 19:01:58 -0800

    OBv2: Deserialize BookSide

---
+++ openbook/src/main/java/com/mmorrell/openbook/model/AnyNode.java
@@ -0,0 +1,39 @@
+package com.mmorrell.openbook.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Builder
+public class AnyNode {
+
+    private byte tag;
+    private byte[] data; // 87 bytes
+
+    public static List<AnyNode> readAnyNodes(byte[] data) {
+        List<AnyNode> nodes = new ArrayList<>();
+
+        int offset = 0;
+        for (int i = 0; i < 1024; i++) {
+            byte newTag = data[offset];
+            offset += 1;
+
+            byte[] newData = Arrays.copyOfRange(data, offset, offset + 7);
+            offset += 7;
+
+            nodes.add(
+                    AnyNode.builder()
+                            .tag(newTag)
+                            .data(newData)
+                            .build()
+            );
+        }
+
+        return nodes;
+    }
+
+}
--- openbook/src/main/java/com/mmorrell/openbook/model/OrderTreeNodes.java
+++ openbook/src/main/java/com/mmorrell/openbook/model/OrderTreeNodes.java
@@ -1,5 +1,10 @@
 package com.mmorrell.openbook.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.mmorrell.openbook.OpenBookUtil;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,9 +12,27 @@ import lombok.Data;
 @Builder
 public class OrderTreeNodes {
 
-    public static OrderTreeNodes readOrderTreeNodes(byte[] data) {
-        OrderTreeNodes orderTreeNodes = new OrderTreeNodes();
+    private byte orderTreeType;
+    // 3 bytes padding
+    private int bumpIndex;
+    private int freeListLen;
+    private int freeListHead;
+    // 512 bytes padding
+    // AnyNode x 1024
+    private List<AnyNode> nodes;
 
-        return orderTreeNodes;
+    public static OrderTreeNodes readOrderTreeNodes(byte[] data) {
+
+        return OrderTreeNodes.builder()
+                .orderTreeType(data[0])
+                .bumpIndex(OpenBookUtil.readInt32(data, 4))
+                .freeListLen(OpenBookUtil.readInt32(data, 8))
+                .freeListHead(OpenBookUtil.readInt32(data, 12))
+                .nodes(
+                        AnyNode.readAnyNodes(
+                                Arrays.copyOfRange(data, 16, data.length)
+                        )
+                )
+                .build();
     }
 }
