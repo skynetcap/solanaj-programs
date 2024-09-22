4fd79adb3e664a9fe6169979c2ea204c1e176ee1
skynetcap <100323448+skynetcap@users.noreply.github.com>
2024-09-21 19:43:30 -0700

    Add new Jupiter module
    Introduced the Jupiter module to the project by updating the root `pom.xml` file and creating the necessary `jupiter/pom.xml`. Added a basic test class `JupiterTest` with a simple proof of concept logging test.

---
+++ jupiter/src/test/java/JupiterTest.java
@@ -0,0 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class JupiterTest {
+
+    @Test
+    public void testProofOfConcept() {
+        log.info("Testing proof of concept");
+    }
+
+}
